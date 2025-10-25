750713ab1cceb4a3efe87633a5a8b190fc5b481e
student-a502 <vasilisaveber7@gmail.com>
2025-10-25 14:44:45 +0500

    Создание классов. Ларионова

---
+++ Classes/Common/CheckRegex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Larionova.Classes.Common
+{
+    public class CheckRegex
+    {
+        public static bool Match(string Pattern, string Input)
+        {
+            Match m = Regex.Match(Input, Pattern);
+            return m.Success;
+        }
+    }
+}
---
+++ Classes/Passport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Larionova.Classes
+{
+    public class Passport
+    {
+
+        public string Name { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Issued { get; set; }
+
+        public string Date0fIssued { get; set; }
+
+        public string DepatmentCode { get; set; }
+
+        public string SerialAndNumber { get; set; }
+
+        public string Date0fBirth { get; set; }
+
+        public string PlaceOfBirth { get; set; }
+    }
+}
+
